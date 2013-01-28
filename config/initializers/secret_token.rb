07d4c9671099722e710ec95f605c4a68ab18a4b8
JT <it@cochs.org>
2013-01-28 09:01:16 -0800

    Generate Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeStudy::Application.config.secret_token = '668e2a5691a9b75bda3632d6011c3ff02267d99c4bb3b309943763be78e838d3f2fa1af2e117e00fe8cc2aec2fe4200ab09586903a315a324476dad19d6c90cd'
