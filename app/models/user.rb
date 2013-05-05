0434d329819f954df231578db3100767044ff153
JT <it@cochs.org>
2013-05-04 20:38:13 -0700

    Prevent user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :rememberable, :recoverable, :token_authenticatable, :confirmable,
-  # :lockable and :omniauthable, :timeoutable
-  devise :database_authenticatable, :registerable,
+  # :lockable and :omniauthable, :timeoutable, :registerable
+  devise :database_authenticatable,
          :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
