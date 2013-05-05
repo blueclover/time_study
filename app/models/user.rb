91325cee39e6eda56f9a2bda8badc1fb386568e3
JT <it@cochs.org>
2013-05-04 19:45:41 -0700

    Change mobile log entry edit page to select boxes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :rememberable, :recoverable, :token_authenticatable, :confirmable,
-  # :lockable and :omniauthable
+  # :lockable and :omniauthable, :timeoutable
   devise :database_authenticatable, :registerable,
-         :trackable, :validatable, :timeoutable
+         :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation,
